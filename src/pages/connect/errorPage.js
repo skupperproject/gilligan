bd34c968e13f839c0553b578e4543def1b5a031c
Ernest Allen <eallen@redhat.com>
2020-07-24 16:16:49 -0400

    Renamed timeoutPage to errorPage and now using it for both connection timeouts and errors

---
+++ src/pages/connect/errorPage.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { LoginPage } from "@patternfly/react-core";
+import {
+  Bullseye,
+  Button,
+  List,
+  ListItem,
+  TextContent,
+  Text,
+  TextVariants,
+} from "@patternfly/react-core";
+import "./connect.css";
+import brandImg from "../../assets/skupper.svg";
+
+class ErrorPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <LoginPage
+        loginTitle={this.props.title}
+        footerListVariants="inline"
+        brandImgSrc={brandImg}
+        brandImgAlt="Gilligan logo"
+        backgroundImgAlt="Images"
+        textContent="A Skupper network management and visualization tool."
+      >
+        <Bullseye>
+          <div className="creating-wrapper">
+            <TextContent>
+              <Text className="creating-message" component={TextVariants.p}>
+                {this.props.error}
+              </Text>
+            </TextContent>
+            <div id="sk-try-list">
+              <TextContent>
+                <Text component={TextVariants.p}>
+                  Here are some things to try:
+                </Text>
+              </TextContent>
+              <List>
+                <ListItem>
+                  <Button
+                    id="sk-try-again"
+                    variant="primary"
+                    onClick={this.props.handleTryAgain}
+                  >
+                    Try again
+                  </Button>
+                </ListItem>
+                <ListItem>
+                  To help diagnose the problem
+                  <pre>{`curl ${window.location.protocol}//${window.location.host}/DATA`}</pre>
+                  and report the results to the skupper support site.
+                </ListItem>
+              </List>
+            </div>
+          </div>
+        </Bullseye>
+      </LoginPage>
+    );
+  }
+}
+
+export default ErrorPage;
