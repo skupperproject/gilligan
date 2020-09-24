155a77015b358191c5eb8854b9efbdfc0cc04917
Ernest Allen <eallen@redhat.com>
2020-09-24 11:34:16 -0400

    Using new custom icon to switch to graph view

--- src/assets/graphIcon.js
+++ src/assets/graphIcon.js
@@ -1,36 +1,39 @@
 import React from "react";
 
 export const GraphIcon = ({
-  width = "40",
-  height = "22",
+  width = "28",
+  height = "20",
   className = "sk-icon-graph",
   scale = 1.0,
 }) => {
   const sscale = `scale(${scale})`;
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
+      version="1.1"
       width={width}
       height={height}
       className={className}
+      xmlns="http://www.w3.org/2000/svg"
     >
       <g transform={sscale}>
-        <g transform="translate(0, 8)">
+        <g id="bezier-path" transform="translate(11, 9)">
+          <path d="M0,0 L 6,6" stroke="black" strokeWidth="1" />
+          <path d="M0,0 L 6,-6" stroke="black" strokeWidth="1" />
+        </g>
+        <g id="service">
           <rect
-            x="2"
-            y="2"
+            x="1"
+            y="6"
             rx="2"
             strokeWidth="1"
             stroke="black"
-            fill="transparent"
+            fill="white"
             width="10"
             height="6"
           />
         </g>
-        <g id="bezier-path" transform="translate(0, 11)">
-          <path d={"M0,0 L"} stroke="black" strokeWidth="1" />
-        </g>
-        <use href="#bezier-path" transform="scale(1,-1)" />
+        <use href="#service" transform="translate(15,-5)" />
+        <use href="#service" transform="translate(15,5)" />
       </g>
     </svg>
   );
