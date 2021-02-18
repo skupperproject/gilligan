4552316d991082fed1e247dbe43cf858dfa3778c
Ernest Allen <eallen@redhat.com>
2021-02-17 20:04:31 -0500

    Don't load DATA.json when there is an error with /DATA unless env variable NODE_ENV=development

--- src/restService.js
+++ src/restService.js
@@ -4,29 +4,70 @@ class RESTService {
     //console.log(`default REST url is ${this.url}`);
   }
 
-  getData = (alt) =>
+  getData = () =>
     new Promise((resolve, reject) => {
       if (process.env.NODE_ENV === "test") {
         const data = require("../public/data/DATA.json");
         resolve(data);
+      } else if (process.env.NODE_ENV === "development") {
+        // This is used to get the data when the console
+        // is served by yarn start or npm start
+        this.fetchFrom("/data/DATA.json")
+          .then(resolve)
+          .catch((error) => {
+            reject(error);
+          });
       } else {
         // try from the window url
         this.fetchFrom(`${this.url}/DATA`)
           .then(resolve)
           .catch((error) => {
-            //console.log("failed to get from window url");
-            // This is used to get the data when the console
-            // is served by yarn start
-            if (!alt) {
-              alt = `/data/DATA.json`;
-            }
-            this.fetchFrom(alt)
-              .then(resolve)
+            reject(error);
+          });
+      }
+    });
+
+  getSiteInfo = () =>
+    new Promise((resolve, reject) => {
+      if (process.env.NODE_ENV === "test") {
+        console.log("getSiteInfo test");
+        const data = require("../public/data/SITE.json");
+        resolve(data);
+      } else {
+        this.fetchFrom(`${this.url}/SITE`)
+          .then(resolve)
+          .catch((error) => {
+            console.log(`getSiteInfo error from ${this.url}/SITE: ${error}`);
+            this.fetchFrom("/data/SITE.json")
+              .then((results) => {
+                console.log(
+                  `getSiteInfo results ${JSON.stringify(results, null, 2)}`
+                );
+                resolve(results);
+              })
               .catch((error) => {
                 reject(error);
               });
           });
       }
+    });
+
+  uploadToken = (data) =>
+    new Promise((resolve, reject) => {
+      fetch(`${this.url}/TOKEN`, {
+        method: "POST",
+        body: data,
+      })
+        .then((response) => {
+          console.log(
+            `uploadToken response is ${JSON.stringify(response, null, 2)}`
+          );
+          resolve(response);
+        })
+        .catch((error) => {
+          console.log(`uploadToken error is ${JSON.stringify(error, null, 2)}`);
+          reject(error);
+        });
     });
 
   fetchFrom = (url) =>
